d0c63c6c343cf8990fd36fdaefbd99052112ca96
Natalia Kazakova <natalia.kazakova@devexpress.com>
2019-06-21 17:02:59 +0300

    add cs solution

---
+++ CS/WindowsFormsMapCustomShape/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace WindowsFormsMapCustomShape
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.dashboardViewer = new DevExpress.DashboardWin.DashboardViewer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dashboardViewer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dashboardViewer
+            // 
+            this.dashboardViewer.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(210)))), ((int)(((byte)(210)))));
+            this.dashboardViewer.Appearance.Options.UseBackColor = true;
+            this.dashboardViewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dashboardViewer.Location = new System.Drawing.Point(0, 0);
+            this.dashboardViewer.Name = "dashboardViewer";
+            this.dashboardViewer.Size = new System.Drawing.Size(800, 450);
+            this.dashboardViewer.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dashboardViewer);
+            this.Name = "Form1";
+            this.Text = "Custom Shape File";
+            ((System.ComponentModel.ISupportInitialize)(this.dashboardViewer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.DashboardWin.DashboardViewer dashboardViewer;
+    }
+}
+
---
+++ CS/WindowsFormsMapCustomShape/Form1.cs
@@ -0,0 +1,45 @@
+﻿using DevExpress.DashboardCommon;
+using DevExpress.DataAccess.Excel;
+using DevExpress.XtraEditors;
+using System.IO;
+
+namespace WindowsFormsMapCustomShape
+{
+    public partial class Form1: XtraForm
+    {
+        public Form1() {
+            InitializeComponent();
+
+            Dashboard dashboard = new Dashboard();
+
+            //Create an Excel data source.
+            DashboardExcelDataSource excelDataSource = new DashboardExcelDataSource();
+            excelDataSource.FileName = @"..\..\Data\Sales.xlsx";
+            ExcelWorksheetSettings worksheetSettings = new ExcelWorksheetSettings("Sheet1", "A1:I4166");
+            excelDataSource.SourceOptions = new ExcelSourceOptions(worksheetSettings);
+            excelDataSource.Fill();
+            dashboard.DataSources.Add(excelDataSource);
+
+            // Create a Choropleth Map dashboard item.
+            ChoroplethMapDashboardItem map = new ChoroplethMapDashboardItem();
+            map.Name = "Choropleth Map";
+            map.DataSource = excelDataSource;
+            map.Legend.Visible = true;
+            map.ShapeTitleAttributeName = "NAME";
+            map.AttributeName = "NAME";
+            map.AttributeDimension = new Dimension("State");
+            ValueMap revenueYTDMap = new ValueMap(new Measure("RevenueYTD (Sum)"));
+            map.Maps.Add(revenueYTDMap);
+
+            // Loads a custom shape file to the map.
+            map.Area = ShapefileArea.Custom;
+            CustomShapefileData data = new CustomShapefileData();
+            data.ShapeData = File.ReadAllBytes(@"..\..\Map\USA.shp");
+            data.AttributeData = File.ReadAllBytes(@"..\..\Map\USA.dbf");
+            map.CustomShapefile.Data = data;
+
+            dashboard.Items.Add(map);
+            dashboardViewer.Dashboard = dashboard;
+        }
+    }
+}
