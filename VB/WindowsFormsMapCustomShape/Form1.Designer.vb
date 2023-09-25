f08669076cd871b9fdd87c4507713b4779ef124f
DevExpressExampleBot <fakeEmail>
2023-09-25 14:35:49 +0400

    Source auto update [skip ci]

--- VB/WindowsFormsMapCustomShape/Form1.Designer.vb
+++ VB/WindowsFormsMapCustomShape/Form1.Designer.vb
@@ -1,59 +1,58 @@
-﻿Namespace WindowsFormsMapCustomShape
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
+Namespace WindowsFormsMapCustomShape
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.components = New System.ComponentModel.Container()
-			Me.dashboardViewer = New DevExpress.DashboardWin.DashboardViewer(Me.components)
-			CType(Me.dashboardViewer, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			' 
-			' dashboardViewer
-			' 
-			Me.dashboardViewer.Appearance.BackColor = System.Drawing.Color.FromArgb((CInt((CByte(210)))), (CInt((CByte(210)))), (CInt((CByte(210)))))
-			Me.dashboardViewer.Appearance.Options.UseBackColor = True
-			Me.dashboardViewer.Dock = System.Windows.Forms.DockStyle.Fill
-			Me.dashboardViewer.Location = New System.Drawing.Point(0, 0)
-			Me.dashboardViewer.Name = "dashboardViewer"
-			Me.dashboardViewer.Size = New System.Drawing.Size(800, 450)
-			Me.dashboardViewer.TabIndex = 0
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(800, 450)
-			Me.Controls.Add(Me.dashboardViewer)
-			Me.Name = "Form1"
-			Me.Text = "Custom Shape File"
-			CType(Me.dashboardViewer, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.components = New System.ComponentModel.Container()
+            Me.dashboardViewer = New DevExpress.DashboardWin.DashboardViewer(Me.components)
+            CType((Me.dashboardViewer), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' dashboardViewer
+            ' 
+            Me.dashboardViewer.Appearance.BackColor = System.Drawing.Color.FromArgb((CInt(((CByte((210)))))), (CInt(((CByte((210)))))), (CInt(((CByte((210)))))))
+            Me.dashboardViewer.Appearance.Options.UseBackColor = True
+            Me.dashboardViewer.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.dashboardViewer.Location = New System.Drawing.Point(0, 0)
+            Me.dashboardViewer.Name = "dashboardViewer"
+            Me.dashboardViewer.Size = New System.Drawing.Size(800, 450)
+            Me.dashboardViewer.TabIndex = 0
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(800, 450)
+            Me.Controls.Add(Me.dashboardViewer)
+            Me.Name = "Form1"
+            Me.Text = "Custom Shape File"
+            CType((Me.dashboardViewer), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+        End Sub
 
-		Private dashboardViewer As DevExpress.DashboardWin.DashboardViewer
-	End Class
+#End Region
+        Private dashboardViewer As DevExpress.DashboardWin.DashboardViewer
+    End Class
 End Namespace
-
--- VB/WindowsFormsMapCustomShape/Form1.vb
+++ VB/WindowsFormsMapCustomShape/Form1.vb
@@ -1,45 +1,41 @@
-﻿Imports DevExpress.DashboardCommon
+Imports DevExpress.DashboardCommon
 Imports DevExpress.DataAccess.Excel
 Imports DevExpress.XtraEditors
 Imports System.IO
 
 Namespace WindowsFormsMapCustomShape
-	Partial Public Class Form1
-		Inherits XtraForm
 
-		Public Sub New()
-			InitializeComponent()
+    Public Partial Class Form1
+        Inherits XtraForm
 
-			Dim dashboard As New Dashboard()
-
-			'Create an Excel data source.
-			Dim excelDataSource As New DashboardExcelDataSource()
-			excelDataSource.FileName = "..\..\Data\Sales.xlsx"
-			Dim worksheetSettings As New ExcelWorksheetSettings("Sheet1", "A1:I4166")
-			excelDataSource.SourceOptions = New ExcelSourceOptions(worksheetSettings)
-			excelDataSource.Fill()
-			dashboard.DataSources.Add(excelDataSource)
-
-			' Create a Choropleth Map dashboard item.
-			Dim map As New ChoroplethMapDashboardItem()
-			map.Name = "Choropleth Map"
-			map.DataSource = excelDataSource
-			map.Legend.Visible = True
-			map.ShapeTitleAttributeName = "NAME"
-			map.AttributeName = "NAME"
-			map.AttributeDimension = New Dimension("State")
-			Dim revenueYTDMap As New ValueMap(New Measure("RevenueYTD (Sum)"))
-			map.Maps.Add(revenueYTDMap)
-
-			' Loads a custom shape file to the map.
-			map.Area = ShapefileArea.Custom
-			Dim data As New CustomShapefileData()
-			data.ShapeData = File.ReadAllBytes("..\..\Map\USA.shp")
-			data.AttributeData = File.ReadAllBytes("..\..\Map\USA.dbf")
-			map.CustomShapefile.Data = data
-
-			dashboard.Items.Add(map)
-			dashboardViewer.Dashboard = dashboard
-		End Sub
-	End Class
+        Public Sub New()
+            InitializeComponent()
+            Dim dashboard As Dashboard = New Dashboard()
+            'Create an Excel data source.
+            Dim excelDataSource As DashboardExcelDataSource = New DashboardExcelDataSource()
+            excelDataSource.FileName = "..\..\Data\Sales.xlsx"
+            Dim worksheetSettings As ExcelWorksheetSettings = New ExcelWorksheetSettings("Sheet1", "A1:I4166")
+            excelDataSource.SourceOptions = New ExcelSourceOptions(worksheetSettings)
+            excelDataSource.Fill()
+            dashboard.DataSources.Add(excelDataSource)
+            ' Create a Choropleth Map dashboard item.
+            Dim map As ChoroplethMapDashboardItem = New ChoroplethMapDashboardItem()
+            map.Name = "Choropleth Map"
+            map.DataSource = excelDataSource
+            map.Legend.Visible = True
+            map.ShapeTitleAttributeName = "NAME"
+            map.AttributeName = "NAME"
+            map.AttributeDimension = New Dimension("State")
+            Dim revenueYTDMap As ValueMap = New ValueMap(New Measure("RevenueYTD (Sum)"))
+            map.Maps.Add(revenueYTDMap)
+            ' Loads a custom shape file to the map.
+            map.Area = ShapefileArea.Custom
+            Dim data As CustomShapefileData = New CustomShapefileData()
+            data.ShapeData = File.ReadAllBytes("..\..\Map\USA.shp")
+            data.AttributeData = File.ReadAllBytes("..\..\Map\USA.dbf")
+            map.CustomShapefile.Data = data
+            dashboard.Items.Add(map)
+            dashboardViewer.Dashboard = dashboard
+        End Sub
+    End Class
 End Namespace
